60badee08818a5c90da55946776fdf0974a6ceec
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-03-10 17:05:31 +0100

    Clickable Drag and Drop incorrecto

---
+++ Assets/Scripts/Mecanicas/DragDrop/ClickableDrag.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableDrag : MonoBehaviour
+{
+    private bool selected;
+
+    private Vector3 initPos;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //comprobacion de permiso
+        if (selected == true)
+        {
+            //coordenadas del raton
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //posicion del objeto sigue las coordenadas del raton todo el rato
+            transform.position = new Vector3(cursorPos.x, cursorPos.y, -5);
+        }
+
+        if (selected && Input.GetMouseButtonDown(0))
+        {
+            // no permitimos seleccion
+            selected = false;
+            //bd
+            this.transform.position = initPos;
+        }
+    }
+    //cuando el cursor esta encima lo detecta
+    void OnMouseOver()
+    {
+        //si se hace click izq
+        if (Input.GetMouseButtonDown(0))
+        {
+            //permitimos seleccion
+            selected = true;
+            initPos = this.transform.position;
+        }
+
+
+    }
+
+
+}
