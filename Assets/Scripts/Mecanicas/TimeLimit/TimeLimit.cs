48086a4195328af8c8cde91af26193565f5bf29e
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-03-10 22:19:05 +0100

    TimeLimit al pasar el tiempo aparece en la pantalla

---
+++ Assets/Scripts/Mecanicas/TimeLimit/TimeLimit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLimit : MonoBehaviour
+{
+    //creamos contador de x segundos y aparece TimeLimit
+    private float contadorTimeLimit = 10;
+
+    //objeto TimeLimit que aparecerá en la pantalla
+    [SerializeField]
+    private GameObject TLimit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        contadorTimeLimit -= Time.deltaTime;
+        Debug.Log(contadorTimeLimit);
+        if(contadorTimeLimit<=0)
+        {
+            //aparece TimeLimit
+            TLimit.SetActive(true);
+        }
+    }
+}
