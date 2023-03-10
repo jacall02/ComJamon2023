5df9251c532db68896a3eb978fc9c019a8a93729
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-03-10 09:31:28 +0100

    Cambio de Escena de Menu a Game

---
+++ Assets/Scripts/Mainmenu/Mainmenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mainmenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambioEscenaGame()
+    {
+        //se pasa a la siguiente escena por orden
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //llamada Game
+        SceneManager.LoadScene("Game");
+    }
+}
