2bbaee1fd9711fcb78fcc51376a08991c11a6c20
Miguel González Pérez <miggon23@ucm.es>
2023-03-10 16:38:38 +0100

    Tornillos caen cuando ven gotas

--- Assets/Scripts/Mecanicas/FallenObject.cs
+++ Assets/Scripts/Mecanicas/FallenObject.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class FallenObject : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         //si el objeto que choca el collider es un destornillador
-        if(collision.gameObject.tag == "Destornillador")
+        if (collision.gameObject.tag == "Destornillador")
         {
             this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
 
