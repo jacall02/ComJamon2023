4afafdff8aead47b5e7837f73fdbae91095c1287
elisatodd <elitodd@ucm.es>
2023-03-11 00:58:36 +0100

    Fade al cambiar de escena

--- Assets/Scripts/Mainmenu/Mainmenu.cs
+++ Assets/Scripts/Mainmenu/Mainmenu.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+using DG.Tweening;
+
 public class Mainmenu : MonoBehaviour
 {
+    public float transitionDuration = 1f;
+    public Ease transitionEase = Ease.InOutQuad;
+
+    [SerializeField] private CanvasGroup canvasGroup;
+
     void Start()
     {
        
@@ -18,10 +25,14 @@ public class Mainmenu : MonoBehaviour
         
     }
 
-    public void CambioEscenaGame()
+    public void CambioEscenaGame(string sceneName)
     {
+        canvasGroup.blocksRaycasts = true;
+
+        canvasGroup.DOFade(0f, transitionDuration).SetEase(transitionEase).OnComplete(()=>SceneManager.LoadScene(sceneName));
+
         //se pasa a la siguiente escena por orden
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         //llamada Game
         //SceneManager.LoadScene("Game");
     }
