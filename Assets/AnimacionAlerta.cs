431859b24d65db67ed3d1fd34fa613efa68f2bf3
elisatodd <elitodd@ucm.es>
2023-03-11 21:42:15 +0100

    Canvas alerta lab

---
+++ Assets/AnimacionAlerta.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacionAlerta : MonoBehaviour
+{
+    [SerializeField] CanvasGroup myCanvas;
+
+    void Start()
+    {
+        myCanvas.DOFade(0.0f, 1.0f).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
