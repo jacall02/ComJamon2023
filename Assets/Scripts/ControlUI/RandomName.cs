2b5c644870e7b6ce0e7ce28fc9e699b6def923ad
Miguel González Pérez <miggon23@ucm.es>
2023-03-11 11:21:16 +0100

    Nombre de archivos en la UI

---
+++ Assets/Scripts/ControlUI/RandomName.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomName : MonoBehaviour
+{
+    [SerializeField]
+    string[] fileNames;
+
+    //[SerializeField]
+    //TextMesh text;
+
+    private TextMeshProUGUI text_;
+
+    private void Start()
+    {
+        text_ = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void ChangeName()
+    {
+        if(text_ != null)
+        {
+            text_.text = GetRandomFileName();
+        }
+    }
+
+    public string GetRandomFileName()
+    {
+        return fileNames[Random.Range(0, fileNames.Length)];
+    }
+}
