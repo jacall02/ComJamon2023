5154365de6cbabd4a950e16d251a786218c05878
Miguel González Pérez <miggon23@ucm.es>
2023-03-11 22:57:39 +0100

    CajonAbotonado

---
+++ Assets/Scripts/Mecanicas/CajonComportamiento.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajonComportamiento : MonoBehaviour
+{
+
+
+    //public void AbrirCajon()
+    //{
+    //    if (cajonAbierto)
+    //    {
+    //        animatorCajonDestornillador.SetBool("abrir", true);
+    //        cajonAbierto = false;
+
+    //    }
+    //}
+
+    //public void CerrarCajon()
+    //{
+    //    if (!cajonAbierto)
+    //    {
+    //        animatorCajonDestornillador.SetBool("abrir", true);
+    //        cajonAbierto = false;
+    //    }
+    //}
+}
