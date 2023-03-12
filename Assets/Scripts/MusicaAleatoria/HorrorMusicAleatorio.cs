616f15ae5c3f2c6d5bfa15635db68ee32d7187f3
PabloGarciaMoreno <123249399+PabletGM@users.noreply.github.com>
2023-03-12 21:09:04 +0100

    musica miedo cada 5 segundos posibilidad

---
+++ Assets/Scripts/MusicaAleatoria/HorrorMusicAleatorio.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorrorMusicAleatorio : MonoBehaviour
+{
+    [SerializeField]
+    private float contadorCambioMusica = 5f;
+
+    [SerializeField]
+    private int posibilidadMusicaMiedo = 2;
+
+    private void Start()
+    {
+        InvokeRepeating("MusicaAleatoria", 5f, 5f);
+    }
+    private void Update()
+    {
+        
+    }
+
+    public void MusicaAleatoria()
+    {
+        if(Random.Range(0,10)<= posibilidadMusicaMiedo)
+        {
+
+            int i = Random.Range(7, 17);
+            SoundManager.instance.PlayEffect(i, 1f);
+        }
+    }
+}
